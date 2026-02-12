61060e57a32556048c47e8efb277374be53c5717
PH <panhe125801@gmail.com>
2026-02-12 08:34:13 +0800

    fitst commit

---
+++ Plugins/WorldState/Source/WorldState/Public/UWorldStateSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "UWorldStateSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORLDSTATE_API UUWorldStateSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/FarmingKingdom/FarmingKingdom.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FarmingKingdom.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FarmingKingdom, "FarmingKingdom" );
